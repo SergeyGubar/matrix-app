ca8f0f70e6b7f61d374510a4d46f878ddd05bd08
Sergey <sergey.gubar187@gmail.com>
2017-10-17 20:03:50 +0300

    Basic fragments work done

--- app/src/main/java/com/example/sergey/matrixandroidtask/MainActivity.java
+++ app/src/main/java/com/example/sergey/matrixandroidtask/MainActivity.java
@@ -1,13 +1,36 @@
 package com.example.sergey.matrixandroidtask;
 
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import fragments.InputFragment;
+import fragments.MatrixFragment;
+import interfaces.MainActivityApi;
+
+public class MainActivity extends AppCompatActivity implements MainActivityApi {
+
+    private static final String MATRIX_SIZE_KEY = "matrixsize";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Fragment fragment = new InputFragment();
+        getSupportFragmentManager()
+                .beginTransaction()
+                .add(R.id.fragment_container, fragment)
+                .commit();
+    }
+
+    @Override
+    public void startMatrixFragment(int size) {
+        Fragment fragment = new MatrixFragment();
+        Bundle args = new Bundle();
+        args.putInt(MATRIX_SIZE_KEY, size);
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.fragment_container, fragment)
+                .commit();
     }
 }
