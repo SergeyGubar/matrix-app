e0ce61cf35d10fa46867b5f1c9c1ae535d37ec5d
Sergey <sergey.gubar187@gmail.com>
2017-10-19 23:04:18 +0300

    Canvas add

--- app/src/main/java/customviews/MatrixView.java
+++ app/src/main/java/customviews/MatrixView.java
@@ -2,13 +2,10 @@ package customviews;
 
 import android.content.Context;
 import android.graphics.Canvas;
-import android.os.Handler;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.util.AttributeSet;
 import android.view.View;
-import android.view.ViewTreeObserver;
-import android.widget.RelativeLayout;
-
-import com.example.sergey.matrixandroidtask.R;
 
 import java.util.Stack;
 
@@ -16,102 +13,43 @@ import java.util.Stack;
  * Created by Sergey on 10/18/2017.
  */
 
-public class MatrixView extends RelativeLayout {
-    private int mHeight;
-    private int mWidth;
-    private int itemDimension;
+public class MatrixView extends View {
+    private Color mColor;
+    private int cellSize = 200;
     private int[][] data;
-    private Handler mHandler;
-    public static int counter = 0;
+    private Paint mPaint;
 
-    public MatrixView(Context context,final int[][] data) {
+    public MatrixView(Context context, final int[][] data) {
         super(context);
-        View.inflate(context, R.layout.matrix_layout, this);
         this.data = data;
-        mHandler = new Handler();
-
-        this.getViewTreeObserver().addOnPreDrawListener(new ViewTreeObserver.OnPreDrawListener() {
-            @Override
-            public boolean onPreDraw() {
-                MatrixView.this.getViewTreeObserver().removeOnPreDrawListener(this);
-                mHeight = MatrixView.this.getHeight();
-                mWidth = MatrixView.this.getWidth();
-                itemDimension = mWidth / data.length;
-                mHandler.post(new Runnable() {
-                    @Override
-                    public void run() {
-                        fillMatrixItems();
-                    }
-                });
-                return false;
-            }
-        });
+        mPaint = new Paint();
+        mPaint.setColor(Color.BLACK);
+        mPaint.setAntiAlias(true);
+        mPaint.setStrokeWidth(1);
+        mPaint.setTextSize(22);
+        mPaint.setStyle(Paint.Style.STROKE);
+        mPaint.setStrokeJoin(Paint.Join.ROUND);
+        mPaint.setStrokeCap(Paint.Cap.ROUND);
     }
-
-    private void fillMatrixItems() {
-        /*for (int i = 0; i < data.length; i++) {
-            for (int j = 0; j < data.length; j++) {
-                counter++;
-                inflateItem(i, j);
-            }
-        }*/
-        int value = 1;
-        int cursor = 0;
-        int counterToCenter;
-        int center = data.length / 2;
-
-
-        for (counterToCenter = 1; counterToCenter <= center; counterToCenter++) {
-
-            for (cursor = counterToCenter - 1; cursor < data.length - counterToCenter + 1; cursor++) {
-                inflateItem(cursor, counterToCenter - 1);
-            }
-
-            for (cursor = counterToCenter; cursor < data.length - counterToCenter + 1; cursor++) {
-                inflateItem(data.length - counterToCenter, cursor);
-            }
-
-            for (cursor = data.length - counterToCenter - 1; cursor >= counterToCenter; cursor--) {
-                inflateItem(cursor, data.length - counterToCenter);
-            }
-
-            for (cursor = data.length - counterToCenter; cursor >= counterToCenter; cursor--) {
-                inflateItem(counterToCenter - 1, cursor);
-            }
-
-        }
-
-        if (data.length % 2 == 1) {
-            inflateItem(center, center);
-        }
-    }
-
-    private void inflateItem(final int i, final int j) {
-        counter++;
-        mHandler.postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                MatrixItem item = new MatrixItem(getContext());
-                item.setMatrixItemText(String.valueOf(data[i][j]));
-                LayoutParams params = new LayoutParams(itemDimension, itemDimension);
-                int leftMargin = j * itemDimension;
-                int topMargin = i * itemDimension;
-                params.setMargins(leftMargin, topMargin, 0, 0);
-                item.setLayoutParams(params);
-                MatrixView.this.addView(item);
-            }
-        }, 2 * counter);
-    }
-
 
 
     public MatrixView(Context context, AttributeSet attrs) {
         super(context, attrs);
-        View.inflate(context, R.layout.matrix_layout, this);
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
-        super.onDraw(canvas);
+//        super.onDraw(canvas);
+        for (int i = 0; i < data.length; i++) {
+            for (int j = 0; j < data.length; j++) {
+                int x = i * cellSize;
+                int y = j * cellSize;
+                canvas.drawRect(x, y, x + cellSize, y + cellSize, mPaint);
+                canvas.drawText(String.valueOf(data[i][j]), i * cellSize + cellSize / 4, j * cellSize + cellSize / 2, mPaint);
+            }
+        }
+
     }
+
+
 }
--- app/src/main/java/fragments/MatrixFragment.java
+++ app/src/main/java/fragments/MatrixFragment.java
@@ -6,13 +6,18 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.telephony.CellIdentityCdma;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.AnimationUtils;
+import android.view.animation.LayoutAnimationController;
 import android.widget.GridLayout;
 import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 
 import com.example.sergey.matrixandroidtask.R;
 
@@ -34,8 +39,9 @@ public class MatrixFragment extends Fragment {
     private static int mDelay;
     private RecyclerView mRecyclerView;
     private MatrixRecyclerAdapter mAdapter;
+    public static int mCellSize = 200;
     private SpiralMatrix mMatrix;
-
+    private static int mMatrixDimension;
 
 
     //end test
@@ -47,22 +53,30 @@ public class MatrixFragment extends Fragment {
         Bundle args = getArguments();
 
         if (args != null && args.containsKey(MATRIX_SIZE_KEY)) {
-            int matrixDimension = args.getInt(MATRIX_SIZE_KEY);
-            mMatrix = new SpiralMatrix(matrixDimension);
+            mMatrixDimension = args.getInt(MATRIX_SIZE_KEY);
+            mMatrix = new SpiralMatrix(mMatrixDimension);
             mDelay = args.getInt(DELAY_KEY);
         }
+
+        RelativeLayout rl = (RelativeLayout) inflatedView.findViewById(R.id.main_container);
+        int[][] arr = mMatrix.getMatrix();
+        MatrixView view = new MatrixView(getContext(), arr);
+        RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(mCellSize * arr.length, mCellSize * arr.length);
+        view.setLayoutParams(params);
+        rl.addView(view);
+/*
 
         mRecyclerView = (RecyclerView) inflatedView.findViewById(R.id.matrix_recycler_view);
         mAdapter = new MatrixRecyclerAdapter(getContext(), mMatrix.getMatrix());
 
-        FixedGridLayoutManager manager = new FixedGridLayoutManager();
-        manager.setTotalColumnCount(mMatrix.getDimension());
+
+        RecyclerView.LayoutManager manager = new GridLayoutManager(getContext(), mMatrixDimension);
         mRecyclerView.setAdapter(mAdapter);
         mRecyclerView.setLayoutManager(manager);
+*/
 
         return inflatedView;
     }
-
 
 
     @Override
